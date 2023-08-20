3ef3842e2d4de6c3fa855efe00a1f363cd15d063
Ivan <ivan.yarmoliuk@gmail.com>
2023-08-20 12:15:52 -0700

    Initial Commit

---
+++ UserFriends/UserFriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserFriendsApp.swift
+//  UserFriends
+//
+//  Created by Ivan Yarmoliuk on 8/20/23.
+//
+
+import SwiftUI
+
+@main
+struct UserFriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
